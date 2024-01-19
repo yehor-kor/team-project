4178f91bab43506d855acbca684a4a23aa703f5e
Yehor Korablin <yegor_ka2004@ukr.net>
2024-01-19 10:33:35 +0200

    Added folders `Front-End` and `Back-End`

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# team-project
+# Team-project-32
+
+## Front-End and Back-end parts of website
---
+++ front-end/src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './App.css';
+
+export const App: React.FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+
+      Hello React!
+    </header>
+  </div>
+);
