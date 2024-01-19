80871e9c9963dd98837d297324a261307f960b8d
Yehor Korablin <yegor_ka2004@ukr.net>
2024-01-19 20:48:08 +0200

    ME-7 Started development of main page

--- front-end/src/components/Header/Header.tsx
+++ front-end/src/components/Header/Header.tsx
@@ -1,5 +1,11 @@
+import './Header.scss';
+
 export const Header: React.FC = () => (
   <header className="Header">
-    Header
+    <nav className="nav">
+      <a href="/" className="logo">
+        <img src="./" alt="Logo" />
+      </a>
+    </nav>
   </header>
 );
